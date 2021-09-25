f59292e5d28ececc99d08b18c0abc5de5a9481ff
RosalieWessels <rosalie.wessels@yahoo.com>
2021-09-25 12:31:07 -0700

    Initial Commit

---
+++ Shared/toDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoListApp.swift
+//  Shared
+//
+//  Created by Rosalie Wessels on 9/25/21.
+//
+
+import SwiftUI
+
+@main
+struct toDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
